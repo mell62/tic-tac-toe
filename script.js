0d0595609ddfd2bd5e4aaed7d49687ee5fd87784
Mridhul <mridhul67@gmail.com>
2023-09-07 00:15:09 +0530

    Create a new function for the game logic

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-const tttGrid = (() => {
-  let tttArray = [
+const gameBoard = (() => {
+  let board = [
     ["X", "O", "X"],
     ["X", "O", "X"],
     ["X", "O", "X"],
   ];
+
+  const getBoard = () => board;
+
+  return { getBoard };
+})();
+
+const displayController = (() => {
+  let board = gameBoard.getBoard();
 
   //DOM cache
   let cells = document.querySelectorAll(".ttt-cell");
 
   //bind events
 
-  function render() {
+  const render = () => {
     let row = 0;
     let column = 0;
     for (let i = 1; i < cells.length + 1; i++) {
-      cells[i - 1].textContent = tttArray[row][column];
+      cells[i - 1].textContent = board[row][column];
       column++;
       if (i % 3 === 0) {
+        // Jump to next row
         row++;
         column = 0;
       }
     }
-  }
+  };
 
   return { render };
 })();
 
-tttGrid.render();
+displayController.render();
